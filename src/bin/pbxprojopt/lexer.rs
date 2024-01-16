b02f01b60a7c7674873556ae1dbccd3ab4f5716c
Seth Junot <xsetech@gmail.com>
2024-01-15 23:43:00 -0800

    Fix: permit trailing comments after dict entries

--- src/bin/pbxprojopt/lexer.rs
+++ src/bin/pbxprojopt/lexer.rs
@@ -113,6 +113,7 @@ mod tests {
         test_case_lexable!("{ \"x\" = \"y\" ; }");
         test_case_lexable!("{ \"x\" /* a */ = \"y\" /* b */ ; }");
         test_case_lexable!("{\nx = 1;\n}");
+        test_case_lexable!("{ x = 1; /* test */ }");
 
         test_case_unlexable!("{;}");
         test_case_unlexable!("{x=}");
