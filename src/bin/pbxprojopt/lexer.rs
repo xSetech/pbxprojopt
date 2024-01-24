00e9d6ee7f86444cc535f60e40e6bf41db59cd7f
Seth Junot <xsetech@gmail.com>
2024-01-24 14:10:42 -0800

    Refactor how tokens and matching rules are formatted

--- src/bin/pbxprojopt/lexer.rs
+++ src/bin/pbxprojopt/lexer.rs
@@ -1,10 +1,94 @@
 //! .pbxproj lexer derivation
 
+use ansi_term::Style;
+use pest::iterators::Pair;
 use pest_derive::Parser as PestParser;
 
 #[derive(PestParser)]
 #[grammar = "pbxproj.pest"]
 pub struct PBXProjLexer;
+
+/// Recursively stylize tokens and matching rules from the lexer. Returns
+/// whether the inner most token was reached. If 'buffer' is None, text will be
+/// printed to stdout. If 'stylize' is true, ANSI color codes will be used.
+///
+pub fn visualize_matched_grammar_rule(
+    pair: &Pair<Rule>,
+    offset: usize,
+    buffer: &mut Option<&mut String>,
+    stylize: bool
+) -> bool {
+
+    let span = pair.clone().as_span();
+    let inner_pairs = pair.clone().into_inner();
+
+    // Format the rule name that generated the token and append the cursor
+    // position if it's the inner-most token.
+    let rule_name: String = format!("{:?}", pair.as_rule());
+    let arrow: &str = if offset != 0 { "->" } else { "" };
+    let position: String = if inner_pairs.len() == 0 {
+        format!("<{},{}>", span.start(), span.end())
+    } else { String::new() };
+
+    let rule_name_style: Style;
+    let location_style: Style;
+    if stylize {
+        rule_name_style = Style::new().bold();
+        location_style = Style::new().dimmed();
+    } else {
+        rule_name_style = Style::new();
+        location_style = Style::new();
+    }
+
+    let formatted = format!(
+        "{}{}{}", arrow, rule_name_style.paint(&rule_name), location_style.paint(&position)
+    );
+
+    match buffer {
+        Some(buf) => buf.push_str(formatted.as_str()),
+        None => print!("{}", formatted),
+    }
+
+    // If the token is a leaf, also append the matching text
+    // e.g. "File->LineComment->LineCommentValue: foo"
+    if inner_pairs.len() == 0 {
+        let leaf_text = format!(": {}", span.as_str().escape_debug());
+        match buffer {
+            Some(buf) => buf.push_str(leaf_text.as_str()),
+            None => print!("{}", leaf_text),
+        }
+        return true;
+    }
+
+    // Format any inner rules/tokens
+    let printed: usize = arrow.len() + rule_name.len() + position.len();
+    let mut first: bool = true;
+    for inner_pair in inner_pairs {
+
+        // Vertically align inner tokens, e.g.
+        // "File->LineComment->LineCommentValue: foo"
+        // "    ->LineComment->LineCommentValue: bar"
+        if first {
+            first = false;
+        } else {
+            let indent = format!("{}", " ".repeat(printed + offset));
+            match buffer {
+                Some(buf) => buf.push_str(indent.as_str()),
+                None => print!("{}", indent),
+            }
+        }
+
+        let need_newline: bool = visualize_matched_grammar_rule(&inner_pair, printed + offset, buffer, stylize);
+        if need_newline {
+            match buffer {
+                Some(buf) => buf.push_str("\n"),
+                None => println!(""),
+            }
+        }
+    }
+
+    return false;
+}
 
 #[cfg(test)]
 mod tests {
--- src/bin/pbxprojopt/pbxprojopt.rs
+++ src/bin/pbxprojopt/pbxprojopt.rs
@@ -7,56 +7,10 @@ pub mod plist;
 use std::fs::read_to_string;
 use std::process::exit;
 
-use ansi_term::Style;
 use clap::Parser as ClapParser;
 use pest::Parser as PestParser;
-use pest::iterators::Pair;
 
-use lexer::PBXProjLexer;
-
-/// Recursively print all matched grammar rules and associated text.
-fn visualize_matched_grammar_rule(pair: &Pair<lexer::Rule>, offset: usize) -> bool {
-    let span = pair.clone().as_span();
-    let inner_pairs = pair.clone().into_inner();
-
-    // Print the rule name and the start/end pos if it's terminal
-    let rule_name: String = format!("{:?}", pair.as_rule());
-    let rule_name_style: Style = Style::new().bold();
-    let text_span_style: Style = Style::new().dimmed();
-    let arrow: &str = if offset != 0 { "->" } else { "" };
-    let text_span: String = if inner_pairs.len() == 0 {
-        format!("<{},{}>", span.start(), span.end())
-    } else { String::new() };
-    print!(
-        "{}{}{}", arrow, rule_name_style.paint(&rule_name), text_span_style.paint(&text_span)
-    );
-
-    // If the rule terminates, also print the corresponding text
-    if inner_pairs.len() == 0 {
-        print!(": {}", span.as_str().escape_debug());
-        return true;
-    }
-
-    // Print all subtokens
-    let printed: usize = arrow.len() + rule_name.len() + text_span.len();
-    let mut first: bool = true;
-    for inner_pair in inner_pairs {
-
-        // Vertically align successive subtokens
-        if first {
-            first = false;
-        } else {
-            print!("{}", " ".repeat(printed + offset));
-        }
-
-        let need_newline: bool = visualize_matched_grammar_rule(&inner_pair, printed + offset);
-        if need_newline {
-            println!("");
-        }
-    }
-
-    return false;
-}
+use lexer::{PBXProjLexer, visualize_matched_grammar_rule};
 
 fn main() {
     let args: arguments::Args = arguments::Args::parse();
@@ -75,7 +29,7 @@ fn main() {
     }
     let lexer_result = lexer_result.unwrap();
     for pair in lexer_result {
-        visualize_matched_grammar_rule(&pair, 0);
+        visualize_matched_grammar_rule(&pair, 0, &mut None, true);
     }
 }
 
